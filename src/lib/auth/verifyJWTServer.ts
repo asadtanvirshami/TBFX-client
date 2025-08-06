87a85656ccbc23c6ea7ee838525cc8819713d328
Asad Tanvir Shami <91515695+asadtanvirshami@users.noreply.github.com>
2025-08-06 14:12:52 +0500

    fix

--- src/lib/auth/verifyJWTServer.ts
+++ src/lib/auth/verifyJWTServer.ts
@@ -1,20 +1,20 @@
 // lib/auth/verifyJWTServer.ts
-import axios from "axios";
+
 import { apiEndpoints } from "@/api/endpoints";
 
 const VERSION = "v1";
 const PRIVACY = "public";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:8080";
-
 export const verifyJWTServer = async (token: string) => {
-  const res = await axios.get(
+  const res = await fetch(
     `${BASE_URL}/${PRIVACY}/api/${VERSION}${apiEndpoints.auth.verifyJWT}`,
     {
+      method: "GET",
       headers: {
         Authorization: `Bearer ${token}`,
       },
-      withCredentials: true,
+      credentials: "include",
     }
   );
-  return res.data;
+  return res.json();
 };
