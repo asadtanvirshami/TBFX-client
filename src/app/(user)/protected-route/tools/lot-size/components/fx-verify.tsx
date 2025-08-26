a74c4f7359a406a47b3852a92c744e4284f16742
Asad Tanvir Shami <91515695+asadtanvirshami@users.noreply.github.com>
2025-08-26 16:08:45 +0500

    push

--- src/app/(user)/protected-route/tools/lot-size/components/fx-verify.tsx
+++ src/app/(user)/protected-route/tools/lot-size/components/fx-verify.tsx
@@ -5,19 +5,7 @@ import Script from "next/script";
 export default function FxVerifyWidget() {
   return (
     <div id="position-size-calculator-624228" className="h-screen">
-      <Script
-        src="https://fxverify.com/Content/remote/remote-widgets.js"
-        strategy="afterInteractive"
-        onLoad={() => {
-          if (typeof window !== "undefined" && window.RemoteCalc) {
-            window.RemoteCalc({
-              Url: "https://fxverify.com",
-              Calculator: "position-size-calculator",
-              ContainerId: "position-size-calculator-624228",
-            });
-          }
-        }}
-      />
+      
     </div>
   );
 }
